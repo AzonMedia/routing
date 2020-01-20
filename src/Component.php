e0c49c14534407721d3fb07d53a3ee3d03cadecc
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-20 17:47:05 +0200

    added component file

---
+++ src/Component.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\Routing;
+
+use Azonmedia\Components\BaseComponent;
+
+class Component extends BaseComponent
+{
+
+    protected const COMPONENT_NAME = "Routing package";
+    protected const COMPONENT_URL = 'https://routing.packages.guzaba.org/';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
+    protected const COMPONENT_VERSION = '0.0.1';
+    protected const VENDOR_NAME = 'Azonmedia';
+    protected const VENDOR_URL = 'https://azonmedia.com';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/routing/docs/ErrorReference/';
+}
